e0fae0f1f39206ef3fc47f586e6244cfa9a2edeb
Benoît Gras <123976487+benoigrs@users.noreply.github.com>
2023-03-25 10:33:23 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2,35 +2,49 @@ import dash
 from dash import Dash, dcc, html, Output, Input
 import plotly.graph_objs as go
 
-with open('value.txt', 'r') as f:
-    values = f.readlines()
 
-current = values[-1]
+file = 'value.txt'
 
-x_values = list(range(len(values) - 1))
-y_values = [float(val.strip()) for val in values[:-1]]
+def read_value():
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    current = lines[-1]
+    data = []
+    for line in lines:
+        parts = line.strip().split(' ')
+        time = parts[0]
+        value = float(parts[1])
+        data.append((time, value))
+    return current, data
 
+def generate_figure(data):
+    x_values = [d[0] for d in data]
+    y_values = [d[1] for d in data]
+    return {
+        'data': [
+            {'x': x_values, 'y': y_values, 'type': 'line'}
+        ],
+        'layout': {
+            'title': 'Historical values',
+            'xaxis': {'title': 'Time'},
+            'yaxis': {'title': 'Value'}
+        }
+    }
 
-app = dash.Dash(__Projet__)
+current, data = read_value()
+latest = current.split(' ')[-1]
+
+app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1('Pernod Ricard Stock'),
-    html.H2('Current: {}'.format(current.strip()), style={'font-size': '40px'}),
+    html.H1('Value Dashboard'),
+    html.H2('Current Pernod Ricard Stock: {}'.format(latest), style={'font-size': '40px'}),
     dcc.Graph(
         id='value-graph',
-        figure={
-            'data': [
-                {'x': x_values, 'y': y_values, 'type': 'line'}
-            ],
-            'layout': {
-                'title': 'Historical values',
-                'xaxis': {'title': 'Time'},
-                'yaxis': {'title': 'Value'}
-            }
-        }
+        figure=generate_figure(data)
     )
 ])
 
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port="8050")
 
-if __name__ == '__Projet__':
-    app.run_server(debug=True,host="0.0.0.0",port="8050")
